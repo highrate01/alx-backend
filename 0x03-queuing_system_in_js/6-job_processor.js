69948508fcab18c275199fb49b1535977a261d3b
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-10-08 23:52:07 +0100

    Create the Job processor

---
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,15 @@
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+// Function to send notification
+function sendNotification(phoneNumber, message) {
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+}
+
+// Process jobs of type 'push_notification_code'
+queue.process('push_notification_code', (job, done) => {
+  const { phoneNumber, message } = job.data;
+  sendNotification(phoneNumber, message);
+  done();
+});
