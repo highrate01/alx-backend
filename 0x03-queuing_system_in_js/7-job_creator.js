256ce3ca31e7cd87c15ef06e777ebc7931f68801
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-10-09 13:38:36 +0100

    Track progress and errors with Kue: Create the Job creator

---
+++ 0x03-queuing_system_in_js/7-job_creator.js
@@ -0,0 +1,46 @@
+import kue from 'kue';
+
+// Array of jobs with phone numbers and messages
+const jobs = [
+  { phoneNumber: '4153518780', message: 'This is the code 1234 to verify your account' },
+  { phoneNumber: '4153518781', message: 'This is the code 4562 to verify your account' },
+  { phoneNumber: '4153518743', message: 'This is the code 4321 to verify your account' },
+  { phoneNumber: '4153538781', message: 'This is the code 4562 to verify your account' },
+  { phoneNumber: '4153118782', message: 'This is the code 4321 to verify your account' },
+  { phoneNumber: '4153718781', message: 'This is the code 4562 to verify your account' },
+  { phoneNumber: '4159518782', message: 'This is the code 4321 to verify your account' },
+  { phoneNumber: '4158718781', message: 'This is the code 4562 to verify your account' },
+  { phoneNumber: '4153818782', message: 'This is the code 4321 to verify your account' },
+  { phoneNumber: '4154318781', message: 'This is the code 4562 to verify your account' },
+  { phoneNumber: '4151218782', message: 'This is the code 4321 to verify your account' }
+];
+
+// Create a Kue queue
+const queue = kue.createQueue();
+
+// Loop through the jobs array and create jobs
+jobs.forEach((jobData) => {
+  const job = queue.create('push_notification_code_2', jobData)
+    .save((err) => {
+      if (err) {
+        console.log(`Notification job creation failed: ${err}`);
+      } else {
+        console.log(`Notification job created: ${job.id}`);
+      }
+    });
+
+  // Event listener for job completion
+  job.on('complete', () => {
+    console.log(`Notification job ${job.id} completed`);
+  });
+
+  // Event listener for job failure
+  job.on('failed', (err) => {
+    console.log(`Notification job ${job.id} failed: ${err}`);
+  });
+
+  // Event listener for job progress
+  job.on('progress', (progress) => {
+    console.log(`Notification job ${job.id} ${progress}% complete`);
+  });
+});
